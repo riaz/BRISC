14d9b15e8828464157c5b7afb65a9202bc384a8c
riaz <riaz.2012@gmail.com>
2025-12-23 06:57:46 +0000

    added example rust code as well

---
+++ rust/rusty-risc/target/debug/deps/rusty_risc-b030883be0c19a73.d
@@ -0,0 +1,5 @@
+/home/orangepi/Projects/BRISC/rust/rusty-risc/target/debug/deps/rusty_risc-b030883be0c19a73.d: src/main.rs
+
+/home/orangepi/Projects/BRISC/rust/rusty-risc/target/debug/deps/rusty_risc-b030883be0c19a73: src/main.rs
+
+src/main.rs:
---
+++ rust/rusty-risc/target/debug/rusty-risc.d
@@ -0,0 +1,1 @@
+/home/orangepi/Projects/BRISC/rust/rusty-risc/target/debug/rusty-risc: /home/orangepi/Projects/BRISC/rust/rusty-risc/src/main.rs
